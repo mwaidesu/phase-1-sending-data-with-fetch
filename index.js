452364a738443406c899af6fbdacf08b2ac7da3f
Mwai <machariamwai@gmail.com>
2022-04-18 16:40:38 +0300

    Pass all tests

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Add your code here
+// const formData = {
+//   dogName : "Byron",
+//   dogBreed: "Poodle",
+// }
+// const configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json",
+//   },
+//   body: JSON.stringify(formData)};
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+// .then(function(response){
+//   return response.json();
+// })
+// .then(function(object){
+//   console.log(object);
+// })
+// .catch(function (error) {
+//   alert("Bad things! Ragnarők!");
+//   console.log(error.message);
+// });
+
+
+function submitData( name, email ) {
+  const data = {
+    name,
+    email
+  }
+  const confObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify( data )
+  }
+  
+  return fetch( 'http://localhost:3000/users', confObj )
+    .then( response => {
+      return response.json()
+    } )
+    .then( object => {
+      document.body.innerHTML = object[ "id" ]
+    } )
+    .catch( error => {
+      document.body.innerHTML = error.message
+    } )
+}
